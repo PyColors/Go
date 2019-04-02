f27de0a35ba21e84f3671adf8904a75cdad93dd8
PyColors <patweb44@gmail.com>
2019-04-02 21:51:16 +0100

    Setting up Deals with multiple returns

---
+++ helloworld.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+type Salutation struct {
+	name     string
+	greeting string
+}
+
+func Greet(salutation Salutation) {
+	// 2 possibilities
+	message, alternate := CreateMessage(salutation.name, salutation.greeting)
+	fmt.Println(message)
+	fmt.Println(alternate)
+}
+
+// Having 2 returns string values (message string, alternate string)
+func CreateMessage(name, greeting string) (message string, alternate string) {
+	message = greeting + " " + name
+	alternate = "Hey" + name
+	return
+}
+
+func main() {
+
+	var s = Salutation{"Bob", "Hello"}
+	Greet(s)
+}
