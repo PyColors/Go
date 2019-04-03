b340263bedee255cc73bf2e7ff7f547223afa10a
PyColors <patweb44@gmail.com>
2019-04-03 23:24:07 +0100

    Passing a Function as a parameter

--- helloworld.go
+++ helloworld.go
@@ -8,22 +8,32 @@ type Salutation struct {
 }
 
 // type + name
-func Greet(salutation Salutation) {
+func Greet(salutation Salutation, do func(string)) {
 	// 2 multiples possibilities
-	message, alternate := CreateMessage(salutation.name, salutation.greeting)
-	fmt.Println(message)
-	fmt.Println(alternate)
+	message, alternate := CreateMessage(salutation.name, salutation.greeting, "coco")
+	do(message)
+	do(alternate)
 }
 
 // Having 2 returns string values (message string, alternate string)
-func CreateMessage(name, greeting string) (message string, alternate string) {
-	message = greeting + " " + name
+func CreateMessage(name string, greeting ...string) (message string, alternate string) {
+	// len() as length
+	fmt.Println(len(greeting))
+	message = greeting[1] + " " + name
 	alternate = "Hey" + name
 	return
+}
+
+func Print(s string) {
+	fmt.Print(s)
+}
+
+func PrintLine(s string) {
+	fmt.Println(s)
 }
 
 func main() {
 
 	var s = Salutation{"Bob", "Hello"}
-	Greet(s)
+	Greet(s, Print)
 }
