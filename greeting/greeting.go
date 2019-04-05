20f352e635f1a644681ade9c06510c648a69c512
PyColors <patweb44@gmail.com>
2019-04-05 23:38:17 +0100

    Add Refactoring, if Statements and new package

---
+++ greeting/greeting.go
@@ -0,0 +1,47 @@
+package greeting
+
+import "fmt"
+
+type Salutation struct {
+	Name     string
+	Greeting string
+}
+
+// Declaring a Function Type - do Printer
+type Printer func(string)
+
+// type + name
+func Greet(salutation Salutation, do Printer, isFormal bool) {
+	// 2 multiples possibilities
+	message, alternate := CreateMessage(salutation.Name, salutation.Greeting)
+	if isFormal {
+		do(message)
+	}
+
+	do(alternate)
+}
+
+// Having 2 returns string values (message string, alternate string)
+func CreateMessage(name, greeting string) (message string, alternate string) {
+	// len() as length
+	fmt.Println(len(greeting))
+	message = greeting + " " + name
+	alternate = "Hey" + name
+	return
+}
+
+func Print(s string) {
+	fmt.Print(s)
+}
+
+func PrintLine(s string) {
+	fmt.Println(s)
+}
+
+// Closure is calling the context
+// Function to create another function
+func CreatePrintFuction(custom string) Printer {
+	return func(s string) {
+		fmt.Println(s + custom)
+	}
+}
--- helloworld.go
+++ helloworld.go
@@ -1,50 +1,9 @@
 package main
 
-import "fmt"
-
-type Salutation struct {
-	name     string
-	greeting string
-}
-
-// Declaring a Function Type - do Printer
-type Printer func(string)
-
-// type + name
-func Greet(salutation Salutation, do Printer) {
-	// 2 multiples possibilities
-	message, alternate := CreateMessage(salutation.name, salutation.greeting, "coco")
-	do(message)
-	do(alternate)
-}
-
-// Having 2 returns string values (message string, alternate string)
-func CreateMessage(name string, greeting ...string) (message string, alternate string) {
-	// len() as length
-	fmt.Println(len(greeting))
-	message = greeting[1] + " " + name
-	alternate = "Hey" + name
-	return
-}
-
-func Print(s string) {
-	fmt.Print(s)
-}
-
-func PrintLine(s string) {
-	fmt.Println(s)
-}
-
-// Closure is calling the context
-// Function to create another function
-func CreatePrintFuction(custom string) Printer {
-	return func(s string) {
-		fmt.Println(s + custom)
-	}
-}
+import "./greeting"
 
 func main() {
 
-	var s = Salutation{"Bob", "Hello"}
-	Greet(s, CreatePrintFuction("??"))
+	var s = greeting.Salutation{"Bob", "Hello"}
+	greeting.Greet(s, greeting.CreatePrintFuction("??"), false)
 }
