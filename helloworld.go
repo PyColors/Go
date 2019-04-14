0dd9c5e50d3dc452b589d71ead0f6e558c123297
Py Colors <patweb44@gmail.com>
2019-04-14 23:40:13 +0100

    Adding comments into the map

--- helloworld.go
+++ helloworld.go
@@ -6,6 +6,17 @@ func main() {
 
 	// var s = greeting.Salutation{"Lisa", "Hello"}
 
+	var s []int
+	// `make()` takes three parameters (array, length, capacity)
+	// Normally `capacity` will be the same as the length
+	// The third parameter is not necessary
+	// 3 is the capacity
+	s = make([]int, 3)
+	s[0] = 1
+	s[0] = 10
+	s[0] = 500
+	s[0] = 23
+
 	slice := []greeting.Salutation{
 		{"Bob", "Hello"},
 		{"Joe", "Hey"},
