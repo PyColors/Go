4a6e31bca1d5fd199179b378b5afa9216072b87e
Py Colors <patweb44@gmail.com>
2019-04-15 23:04:33 +0100

    Short Slice syntax

--- helloworld.go
+++ helloworld.go
@@ -6,17 +6,18 @@ func main() {
 
 	// var s = greeting.Salutation{"Lisa", "Hello"}
 
-	var s []int
-
 	// `make()` takes three parameters (array, length, capacity)
 	// Normally `capacity` will be the same as the length
 	// The third parameter is not necessary
 	// 3 is the capacity
-	s = make([]int, 3)
-	s[0] = 1
-	s[0] = 10
-	s[0] = 500
-	s[0] = 23
+	//s = make([]int, 3)
+	//s[0] = 1
+	//s[0] = 10
+	//s[0] = 500
+	//s[0] = 23
+
+	// Short Slice syntax
+	s := []int{1, 10, 500, 25}
 
 	slice := []greeting.Salutation{
 		{"Bob", "Hello"},
